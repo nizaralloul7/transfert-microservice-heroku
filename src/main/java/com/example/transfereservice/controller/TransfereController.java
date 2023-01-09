6ab9963815036bb62575bdb269c89d1ffea717ca
nizaralloul7 <nizaralloul7@outlook.fr>
2023-01-09 22:48:44 +0100

    other commit

--- src/main/java/com/example/transfereservice/controller/TransfereController.java
+++ src/main/java/com/example/transfereservice/controller/TransfereController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path ="/api/transfere")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TransfereController
 {
     private final TransfereService transfereService;
